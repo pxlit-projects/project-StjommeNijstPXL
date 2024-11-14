b6ee58ead3dcc1e3a1c210dc9a5b98d2f92d5a8f
StjommeNijstPXL <stjomme.nijst@student.pxl.be>
2024-11-14 15:03:38 +0100

    opstarten van frontend + validatie createPost

--- backend-java/Backend-StjommeNijst/post-service/src/main/java/be/pxl/services/controller/PostController.java
+++ backend-java/Backend-StjommeNijst/post-service/src/main/java/be/pxl/services/controller/PostController.java
@@ -3,8 +3,10 @@ package be.pxl.services.controller;
 import be.pxl.services.domain.Dto.PostRequest;
 import be.pxl.services.domain.Post;
 import be.pxl.services.service.PostService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,8 +17,15 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Post createPost(@RequestBody PostRequest postRequest) {
-        return postService.createPost(postRequest);
+    public ResponseEntity<String> createPost(@RequestBody PostRequest postRequest, HttpServletRequest request) {
+        String role = request.getHeader("X-User-Role");
+
+        if ("redacteur".equalsIgnoreCase(role)) {
+            Post createdPost = postService.createPost(postRequest);
+            return ResponseEntity.ok("Post created with ID: " + createdPost);
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("User does not have permission to create posts.");
+        }
     }
+
 }
